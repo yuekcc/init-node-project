ca49d7c6749190d141323bcbf2a159de3bdb8155
yuekcc <yuekcc@noreply.codeberg.org>
2021-09-13 07:58:37 +0200

    add .editorconfig

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ const projectName = path.basename(process.cwd());
 packageTemplate.name = projectName;
 
 fs.writeFileSync('package.json', JSON.stringify(packageTemplate, null, 2), 'utf-8');
+
+const editorconfig = `
+root = true
+
+[*]
+indent_size = 2
+indent_style = space
+`
+
+fs.writeFileSync('.editorconfig', JSON.stringify(editorconfig, null, 2), 'utf-8');
