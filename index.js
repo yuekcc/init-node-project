1a612cb51416e89dc28f47b3f36a995feb2d2bbc
yuekcc <yuekcc@qq.com>
2021-09-15 22:53:29 +0800

    fix template error

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const fs = require('fs');
 const projectName = path.basename(process.cwd());
 packageTemplate.name = projectName;
 
+delete packageTemplate.bin;
+
 fs.writeFileSync('package.json', JSON.stringify(packageTemplate, null, 2), 'utf-8');
 
-const editorconfig = `
-root = true
+const editorconfig = `root = true
 
 [*]
 indent_size = 2
 indent_style = space
-`
+`;
 
-fs.writeFileSync('.editorconfig', JSON.stringify(editorconfig, null, 2), 'utf-8');
+fs.writeFileSync('.editorconfig', editorconfig, 'utf-8');
